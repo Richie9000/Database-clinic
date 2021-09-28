eed19c58fa1ec0b7831199c645b41846e6f8d1c8
Rhyine Stewart <rhyinestewart@gmail.com>
2021-09-28 16:28:47 -0500

    Add new SQL commands

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -36,3 +36,23 @@ treatment_id int NULL,
 CONSTRAINT invoice_items_pk PRIMARY KEY (id),
 CONSTRAINT invoice_items_fk FOREIGN KEY (treatment_id) REFERENCES treatments(id) ON DELETE CASCADE ON UPDATE CASCADE
 ); 
+
+CREATE TABLE public.invoices (
+id int NOT NULL GENERATED ALWAYS AS IDENTITY,
+total_amount decimal NULL,
+generated_at timestamp NULL,
+payed_at timestamp NULL,
+medical_history_id int NULL,
+CONSTRAINT invoices_pk PRIMARY KEY (id),
+CONSTRAINT invoices_fk FOREIGN KEY (medical_history_id) REFERENCES public.medical_histories(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+ALTER TABLE public.invoice_items ADD CONSTRAINT invoice_items_fk_1 FOREIGN KEY (invoice_id) REFERENCES public.invoices(id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+CREATE TABLE public.medical_histories_treatments (
+treatment_id int NULL,
+medical_history_id int NULL,
+CONSTRAINT medical_histories_treatments_pk PRIMARY KEY (treatment_id,medical_history_id),
+CONSTRAINT medical_histories_treatments_fk_1 FOREIGN KEY (treatment_id) REFERENCES public.treatments(id) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT medical_histories_treatments_fk FOREIGN KEY (medical_history_id) REFERENCES public.medical_histories(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
